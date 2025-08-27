595cfbc51a01355de2f65537dcdda5fcc972cf84
Sidharth <sidharths550@gmail.com>
2025-08-27 19:04:02 +0530

    add: Flight Repo

--- src/main/java/com/projects/flightbooking/repository/FlightRepository.java
+++ src/main/java/com/projects/flightbooking/repository/FlightRepository.java
@@ -1,10 +1,33 @@
 package com.projects.flightbooking.repository;
 
 import com.projects.flightbooking.entity.Flight;
+import com.projects.flightbooking.entity.enums.FlightStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight,Long> {
+    @Query("SELECT f from Flight f WHERE f.originAirport =:origin "+
+            "AND f.destinationAirport = :destination " +
+            "AND DATE(f.departureTime) = DATE(:departureDate) "+
+            "AND f.status = :status "+
+            "AND f.availableSeats > 0"+
+            "ORDER BY f.departureTime")
+    List<Flight> findAvailableFlights(@Param("origin") String origin,
+                                      @Param("destination") String destination,
+                                      @Param("departureDate")LocalDateTime departureDate,
+                                      @Param("status")FlightStatus status
+                                      );
 
+    Optional<Flight> findByFlightNumber(String flightNumber);
+
+    @Query("SELECT f FROM Flight f WHERE f.departureTime BETWEEN :startTime AND :endTime")
+    List<Flight> findFlightsByTimeRange(@Param("startTime")LocalDateTime startTime,
+                                        @Param("endTime") LocalDateTime endTime);
 }
