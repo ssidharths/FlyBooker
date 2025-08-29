e12205d05f6524d4b92962f50847435237d65de5
Sidharth <sidharths550@gmail.com>
2025-08-30 00:10:57 +0530

    add: every seat related ops in seat service

--- src/main/java/com/projects/flightbooking/controller/SeatController.java
+++ src/main/java/com/projects/flightbooking/controller/SeatController.java
@@ -1,5 +1,7 @@
 package com.projects.flightbooking.controller;
 
+import com.projects.flightbooking.service.SeatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,4 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/api/v1/seats")
 @Tag(name = "Seat Management", description = "APIs for seat selection and management")
 public class SeatController {
+    @Autowired
+    SeatService seatService;
+
 }
